e1d7662ae72e5e366cae3440ddbe1660b7e08a02
KurniawanAdi12 <kurniawanadi4556@gmail.com>
2023-05-27 20:06:37 +0700

    membuat create new post page

---
+++ blogger/create-post.php
@@ -0,0 +1,27 @@
+<?php include('components/header.php'); ?>
+
+<section class="py-5">
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between mb3">
+            <h4 class="text-primmary fw-bold">Create New Post</h4>
+            <a href="posts.php" class="btn btn-light">Go Page</a>
+        </div>
+        <form action="#" method="post">
+            <div class="mb-3">
+                <label for="title">Title Post</label>
+                <input type="text" name="title" placeholder="title your post" id="title" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="body">Body</label>
+                <textarea name="body" cols="30" rows="10" placeholder="descripsion" id="body" class="form-control"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="image">Image</label>
+                <input type="file" name="image" id="image" class="form-control">
+            </div>
+            <button class="btn btn-primary px-4">Submit</button>
+        </form>
+    </div>
+</section>
+
+<?php include('components/footer.php'); ?>
