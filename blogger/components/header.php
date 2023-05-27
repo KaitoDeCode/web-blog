c7a83a728f1c084de957cc756afa2c2f6262fad2
KurniawanAdi12 <kurniawanadi4556@gmail.com>
2023-05-27 17:47:08 +0700

    Membuat Dashboard

---
+++ blogger/components/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLOG APP </title>
+    <link rel="stylesheet" href="../assets/vendors/css/style.css">
+    <link rel="stylesheet" href="../assets/vendors/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg bg-white py-3">
+    <div class="container">
+        <a href="#" class="navbar-brand text-uppercase">Blog.Dev</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navMenu">
+            <ul class="navbar-nav gap-3 me-auto">
+                <li class="nav-item"><a href="." class="nav-link active">Dashboard</a></li>
+                <li class="nav-item"><a href="." class="nav-link">Posts</a></li>
+                <li class="nav-item"><a href="." class="nav-link">Create Post</a></li>
+                <li class="nav-item"><a href="." class="nav-link">Profile</a></li>
+            </ul>
+            <ul class="navbar-nav gap-3">
+            <li class="nav-item"><p class="nav-link active">Adi Kurniawan</p></li>
+            <li class="nav-item"><a href="login.php" class="nav-link">Log Out</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- end of Navigation -->
---
+++ blogger/index.php
@@ -0,0 +1,9 @@
+<?php include('components/header.php'); ?>
+
+<section class="py-5">
+    <div class="container">
+        <h2 class="section-title text-primmary">Welcome back, Adi Kurniawan</h2>
+    </div>
+</section>
+
+<?php include('components/footer.php'); ?>
