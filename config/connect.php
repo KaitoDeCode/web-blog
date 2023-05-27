0f9136a479402aa093fa3100c95154d7da4682ae
KurniawanAdi12 <kurniawanadi4556@gmail.com>
2023-05-27 20:41:13 +0700

    membangun koneksi ke database

---
+++ config/connect.php
@@ -0,0 +1,13 @@
+<?php
+// $host = "localhost";
+// $username = "root";
+// $password = "";
+// $db = "blog";
+
+// $connect = mysqli_connect($host, $username, $password, $db);
+
+// cara lama yaitu kita harus membuat variable terlebih dahulu dan dimaksukan kedalam variable connect
+
+
+$connect = mysqli_connect("localhost", "root", "", "blog");
+// ini adalah cara gampang yaitu dengan langsung memasukan value yang dulu harus kita masukan terlebih dahulu ke variable
