a1abf2eaf8de8c326143209935dc176b2feff710
KurniawanAdi12 <kurniawanadi4556@gmail.com>
2023-05-27 17:28:27 +0700

    membuat register dan login page

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php include('components/header.php')?>
+
+<section class="auth py-5">
+    <div class="container">
+        <h2 class="text-primmary text-center mb-4">Login</h2>
+        <form action="#" method="post">
+            <div class="mb-3">
+                <label for="email">Email</label>
+                <input placeholder="Enter your email" type="email" name="email" id="email" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="password">Password</label>
+                <input placeholder="Enter your password" type="password" name="password" id="password" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary w-100" name="submit">Login</button>
+            <p class="mt-3 text-secondary text-center">Don't have an account? <a class="text-primmary" href="register.php">Register!</a></p>
+        </form>
+    </div>
+</section>
---
+++ register.php
@@ -0,0 +1,23 @@
+<?php include('components/header.php')?>
+
+<section class="auth py-5">
+    <div class="container">
+        <h2 class="text-primmary text-center mb-4">Register</h2>
+        <form action="#" method="post">
+            <div class="mb-3">
+                <label for="name">Full name</label>
+                <input placeholder="Enter your name" type="text" name="name" id="name" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="email">Email</label>
+                <input placeholder="Enter your email" type="email" name="email" id="email" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="password">Password</label>
+                <input placeholder="Enter your password" type="password" name="password" id="password" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary w-100" name="submit">Register</button>
+            <p class="mt-3 text-secondary text-center">Sudah punya akun? <a class="text-primmary" href="login.php">Login!</a></p>
+        </form>
+    </div>
+</section>
