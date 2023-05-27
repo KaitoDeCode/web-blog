2393b2114890fd5320e6d30888d5a56999fb736c
KurniawanAdi12 <kurniawanadi4556@gmail.com>
2023-05-27 16:17:37 +0700

    homepage part 2

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLOG APP </title>
+    <link rel="stylesheet" href="./assets/vendors/css/style.css">
+    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg bg-white py-3">
+    <div class="container">
+        <a href="#" class="navbar-brand text-uppercase">Blog.Dev</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navMenu">
+            <ul class="navbar-nav gap-3 mx-auto">
+                <li class="nav-item"><a href="." class="nav-link active">Home</a></li>
+                <li class="nav-item"><a href="." class="nav-link">Blog</a></li>
+                <li class="nav-item"><a href="." class="nav-link">Services</a></li>
+                <li class="nav-item"><a href="." class="nav-link">About Us</a></li>
+            </ul>
+            <ul class="navbar-nav gap-3">
+            <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+            <li class="nav-item"><a href="#" class="btn btn-primary rounded-pill">Register</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- end of Navigation -->
+
+<!-- Header -->
+<header class="py-5">
+<div class="container">
+    <h1 class="header-title text-primmary text-center text-capitalize">Discover Nice Article here</h1>
+    <p class="text-secondary text-center text-capitalize">
+        all the article and content of the side have been Update. today and you can find your article and contents quickly and without any problems.
+    </p>
+    <form action="" method="get" class="d-flex align-items-center gap-2 bg-light rounded p-2">
+        <div class="input-group">
+            <span class="input-grup-text bg-transparent border-3">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+            </span>
+            <input type="search" class="form-control" name="search" id="search" placeholder="search">
+        </div>
+        <button type="submit" class="btn btn-primary">Go</button>
+    </form>
+</div>
+</header>
+<!-- end of Header -->
+
+<!-- Hero Post -->
+<section class="py-5">
+    <div class="container">
+        <a href="." class="row align-items hero-post">
+            <div class="col-md-6">
+                <img src="./assets/images/post/post-1.jpg" alt="hero post" class="rounded-3 mb-3 hero-post-img">
+            </div>
+            <div class="col-md-6">
+                <span class="bg-light rounded p-2 fw-bold text-primmary category">The Newest</span>
+                <h2 class="hero-post-title text-primmary textcapitalize mt-3">
+                Finding Temporal Patterns in Twitter Posts: Exploratory Data Analysis with Python
+                </h2>
+                <p class="text-secondary">
+                Clustering of Twitter data with Python, K-Means, and t-SNE — In the article “What People Write about Climate” I analyzed Twitter posts
+                </p>
+                <div class="d-flex alignt-itmes-center gap-3">
+                    <img src="./assets/images//user/adi.jpg" alt="adi kurniawan" class="avatar rounded-circle">
+                    <div class="profile">
+                        <p class="m-0 text-primmary">Adi Kurniawan</p>
+                        <p style="font-size: 14px;"
+                         class="m-0 text-secondary">Web Developert</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+</section>
+<!-- end of Hero Post -->
+
+<section class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <a href="#" class="card card-post border-0 rounded-3 mb-3">
+                    <img src="https://dl.kaskus.id/i.ibb.co/BnWg5qD/banner-dc.jpg" alt="article" class="card-img-top">
+                    <div class="card-body">
+                    <span class="bg-light rounded p-2 fw-bold text-primmary category">The Newest</span>
+                <h2 class="card-post-title text-primmary text-capitalize mt-3">
+                REFRESH RAGNAROK SEGERA HADIR DENGAN SEJUMLAH HADIAH MENARIK DAN PETUALANGAN SERUU!
+                </h2>
+                <p class="text-secondary">
+               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, itaque!
+                </p>
+                <div class="d-flex alignt-itmes-center gap-3">
+                    <img src="./assets/images//user/adi.jpg" alt="adi kurniawan" class="avatar rounded-circle">
+                    <div class="profile">
+                        <p class="m-0 text-primmary">Adi Kurniawan</p>
+                        <p style="font-size: 14px;"
+                         class="m-0 text-secondary">Web Developert</p>
+                    </div>
+                </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="card card-post border-0 rounded-3 mb-3">
+                    <img src="https://s.kaskus.id/r480x480/images/2023/05/25/10996683_202305250436260241.jpg" alt="article" class="card-img-top">
+                    <div class="card-body">
+                    <span class="bg-light rounded p-2 fw-bold text-primmary category">The Newest</span>
+                <h2 class="card-post-title text-primmary text-capitalize mt-3">
+                Baiknya Orang Tegal Sampai Bikin Biksu Thailand Menangis, Jadilah Seperti Orang Tegal
+                </h2>
+                <p class="text-secondary">
+               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, itaque!
+                </p>
+                <div class="d-flex alignt-itmes-center gap-3">
+                    <img src="./assets/images//user/adi.jpg" alt="adi kurniawan" class="avatar rounded-circle">
+                    <div class="profile">
+                        <p class="m-0 text-primmary">Adi Kurniawan</p>
+                        <p style="font-size: 14px;"
+                         class="m-0 text-secondary">Web Developert</p>
+                    </div>
+                </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="card card-post border-0 rounded-3 mb-3">
+                    <img src="https://s.kaskus.id/r480x480/images/2023/05/21/10941161_202305210647320188.jpg" alt="article" class="card-img-top">
+                    <div class="card-body">
+                    <span class="bg-light rounded p-2 fw-bold text-primmary category">The Newest</span>
+                <h2 class="card-post-title text-primmary text-capitalize mt-3">
+                Poto Batu, Pantai Batu Bolong di Kabupaten Sumbawa Barat
+                </h2>
+                <p class="text-secondary">
+               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, itaque!
+                </p>
+                <div class="d-flex alignt-itmes-center gap-3">
+                    <img src="./assets/images//user/adi.jpg" alt="adi kurniawan" class="avatar rounded-circle">
+                    <div class="profile">
+                        <p class="m-0 text-primmary">Adi Kurniawan</p>
+                        <p style="font-size: 14px;"
+                         class="m-0 text-secondary">Web Developert</p>
+                    </div>
+                </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="py-5">
+    <div class="container">
+        <hr class="mb-3">
+        <div class="row align items-center justify-content-between">
+            <div class="col-md-4">
+                <ul class="list-unstyled d-flex align-items-center justify-content-center justify-content-md-start gap-3 mb-3 mb-md-0">
+                    <li><a href="#" class="text-primmary">Home</a></li>
+                    <li><a href="#" class="text-primmary">Blog</a></li>
+                    <li><a href="#" class="text-primmary">Services</a></li>
+                    <li><a href="#" class="text-primmary">About Ss</a></li>
+                    <li><a href="#" class="text-primmary">Contact US</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="text-primmary fw-bolder fs-4 text-uppercase text-center d-block mb-3 mb-md-0">
+                    Blog.Dev
+                </a>
+            </div>
+            <div class="col-md-4">
+                <p class="m-0 text-secondary text-center text-md-end "> &copy; 2023. All Right Reversed</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
